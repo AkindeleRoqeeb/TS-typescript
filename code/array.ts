ef51c0d0673ffc274604a3ec79b6dc7bbdc881fe
AKINDELE ABDULROQEEB <roqeebakindele@gmail.com>
2023-11-19 17:33:46 +0000

    array

--- code/array.ts
+++ code/array.ts
@@ -17,4 +17,54 @@ const allUser: User[] = []
 
 allUser.push({name: "", isactive: false})
 
+// ////////////////////////
+
+const MLModel: number[][] = [
+    [23, 43, 66],
+    [87, 43, 98],
+    []
+]
+
+let score: number | string = 56
+
+score = 9782
+score = "money"
+
+// //////////////////////
+
+type UserDB = {
+    name : string,
+    id : number
+}
+
+type Admin = {
+    username : string,
+    id : number
+}
+
+let userDBinfo: UserDB | Admin = {
+    name: "abdul", id: 873
+}
+
+userDBinfo = {username: "man", id: 654}
+
+
+// function getDBid(id: number | string){
+//     // id.toupperCase()
+//     console.log("manner of life")
+//     return `this your ${id}`
+// }
+getDBid(873)
+getDBid("life you all")
+
+function getDBid(id: number | string){
+    // id.toLowerCase()
+    if (typeof id === "string") {
+        id.toLowerCase()
+    }
+}
+
+
+// getDBID(356)
+
 export {}
